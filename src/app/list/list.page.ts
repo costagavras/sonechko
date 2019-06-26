8c67257b47a3d1877bd5d2a7a79315656efb44c9
Ekdeep <ekdeep.deol@hotmail.com>
2019-06-25 20:10:30 -0400

    alla test

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ListPage implements OnInit {
   private selectedItem: any;
   private icons = [
-    'man'
+    'man', 'alla'
   ];
   public items: Array<{ title: string; note: string; icon: string }> = [];
   constructor() {
